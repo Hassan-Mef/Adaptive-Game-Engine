07ca458df6b2c1c721a43e658423ba5ceb598df8
Hassan-Mef <hassanmef19@gmail.com>
2025-11-28 01:00:24 +0500

    shooting and spawning mulitple targets

--- src/game/components/Target.jsx
+++ src/game/components/Target.jsx
@@ -1,3 +1,25 @@
-export default function Target() {
-  return null;
-}
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
+
+const Target = forwardRef(({ position }, ref) => {
+  const meshRef = useRef();
+
+  // Expose a method to move/respawn the target
+  useImperativeHandle(ref, () => ({
+    respawn: () => {
+      const randomX = (Math.random() - 0.5) * 6;  // -3 to +3
+      const randomY = Math.random() * 2 + 1.5;    // 1.5 to 3.5
+      const randomZ = -(Math.random() * 7 + 5);   // -5 to -12
+      meshRef.current.position.set(randomX, randomY, randomZ);
+    },
+    getMesh: () => meshRef.current
+  }));
+
+  return (
+    <mesh ref={meshRef} position={position}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="red" />
+    </mesh>
+  );
+});
+
+export default Target;
--- src/game/scenes/AimTrainingScene.jsx
+++ src/game/scenes/AimTrainingScene.jsx
@@ -1,9 +1,49 @@
-import { use, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { PointerLockControls } from "@react-three/drei";
 import useFPSControls from "../hooks/useFPSControls";
+import { Raycaster } from "three";
+import * as THREE from "three";
+import { useThree } from "@react-three/fiber";
+import Target from "../components/Target";
 
 export default function AimTrainingScene() {
-  const cubeRef = useRef();
+ 
+  const { camera } = useThree();
+  const scoreRef = useRef(0);
+
+  const targetRefs = [ useRef(), useRef(), useRef() ];
+
+  const handleShoot = () => {
+    const raycaster = new THREE.Raycaster(); // https://threejs.org/docs/?q=raycas#Raycaster
+
+    const cameraDirection = new THREE.Vector3();
+    // getting camera direction
+    camera.getWorldDirection(cameraDirection);
+    cameraDirection.normalize();
+
+    // set ray origin and direction
+    raycaster.set(camera.position, cameraDirection);
+
+    targetRefs.forEach((targetRef) => {
+    const hits = raycaster.intersectObject(targetRef.current.getMesh());
+    if (hits.length > 0) {
+      console.log("Hit!");
+      scoreRef.current += 1;
+      console.log("Score:", scoreRef.current);
+
+      targetRef.current.respawn();
+      }
+    })
+  };
+
+  useEffect(() => {
+    const shoot = () => handleShoot();
+    window.addEventListener("mousedown", handleShoot);
+    return () => {
+      window.removeEventListener("mousedown", handleShoot);
+    };
+  }, []);
+
   useFPSControls();
 
   return (
@@ -13,10 +53,9 @@ export default function AimTrainingScene() {
       <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
 
       {/* Cube target */}
-      <mesh ref={cubeRef} position={[0, 1, -5]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="red" />
-      </mesh>
+      <Target ref={targetRefs[0]} position={[0, 2, -10]} />
+      <Target ref={targetRefs[1]} position={[-2, 3, -8]} />
+      <Target ref={targetRefs[2]} position={[2, 1.5, -12]} />
 
       {/* FPS Controls */}
       <PointerLockControls />
