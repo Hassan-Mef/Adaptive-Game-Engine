2ad1c70827e68c5271ccffbde9320164e644e580
Hassan-Mef <hassanmef19@gmail.com>
2025-12-18 09:18:22 +0500

    Switch player API to stored procedure

--- backend/src/controllers/player.controller.js
+++ backend/src/controllers/player.controller.js
@@ -1,4 +1,33 @@
-const { poolPromise } = require('../config/db');
+// const { poolPromise } = require('../config/db');
+
+// exports.getPlayerById = async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     const pool = await poolPromise;
+
+//     // TEMP query (we will replace with SP)
+//     const result = await pool
+//       .request()
+//       .input('PlayerID', id)
+//       .query('SELECT @PlayerID AS PlayerID');
+
+//     res.status(200).json({
+//       success: true,
+//       data: result.recordset
+//     });
+
+//   } catch (err) {
+//     res.status(500).json({
+//       success: false,
+//       error: err.message
+//     });
+//   }
+// };
+
+
+
+const { poolPromise, sql } = require('../config/db');
 
 exports.getPlayerById = async (req, res) => {
   const { id } = req.params;
@@ -6,11 +35,10 @@ exports.getPlayerById = async (req, res) => {
   try {
     const pool = await poolPromise;
 
-    // TEMP query (we will replace with SP)
     const result = await pool
       .request()
-      .input('PlayerID', id)
-      .query('SELECT @PlayerID AS PlayerID');
+      .input('PlayerID', sql.Int, id)
+      .execute('sp_GetPlayerById');
 
     res.status(200).json({
       success: true,
